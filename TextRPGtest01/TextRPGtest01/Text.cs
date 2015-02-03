2bbdad07a326da0996ac05e1231e22e60bf92b37
Anthony <anthogame57@yahoo.fr>
2015-02-03 09:44:14 +0100

    textRpgtest01
    slow text test c# vs

---
+++ TextRPGtest01/TextRPGtest01/Text.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPGtest01
+{
+    class Text
+    {
+        public string text1 = "Some text";
+        public string textWelcome = "Welcome to Stimuli Control";
+    }
+}
